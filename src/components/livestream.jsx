d41d1e801e4833d447f809162c1815c8236c7377
Lana Zhang <lanaz@amazon.com>
2023-03-09 15:47:15 -0500

    update livestream page

--- src/components/livestream.jsx
+++ src/components/livestream.jsx
@@ -9,12 +9,11 @@ class LiveStream extends React.Component {
     constructor(props) {
         super(props);
         this.moderationLables = [
-            {"Timestamp": 1500, "ModerationLabel": {"Confidence": 70.20341491699219, "Name": "Violence", "ParentName": ""}}, 
-            {"Timestamp": 18500, "ModerationLabel": {"Confidence": 70.20341491699219, "Name": "Violence", "ParentName": ""}}, 
-            {"Timestamp": 18500, "ModerationLabel": {"Confidence": 70.20341491699219, "Name": "Weapon Violence", "ParentName": "Violence"}}, 
-            {"Timestamp": 51600, "ModerationLabel": {"Confidence": 69.24276733398438, "Name": "Violence", "ParentName": ""}}, 
-            {"Timestamp": 51600, "ModerationLabel": {"Confidence": 69.24276733398438, "Name": "Weapon Violence", "ParentName": "Violence"}}
-        ];
+            {"Timestamp": 17000, "ModerationLabel": {"Confidence": 75.54669189453125, "Name": "Weapon Violence", "ParentName": "Violence"}}, 
+            {"Timestamp": 18483, "ModerationLabel": {"Confidence": 68.30712890625, "Name": "Weapon Violence", "ParentName": "Violence"}}, 
+            {"Timestamp": 50716, "ModerationLabel": {"Confidence": 67.32745361328125, "Name": "Weapon Violence", "ParentName": "Violence"}}, 
+            {"Timestamp": 51566, "ModerationLabel": {"Confidence": 84.10609436035156, "Name": "Weapon Violence", "ParentName": "Violence"}}, 
+            {"Timestamp": 51716, "ModerationLabel": {"Confidence": 83.47505187988281, "Name": "Weapon Violence", "ParentName": "Violence"}}];
         this.videoRef = React.createRef();
         this.webCamRef = React.createRef();
         this.state = {
@@ -28,7 +27,7 @@ class LiveStream extends React.Component {
         setInterval(() => {
             if (this.webCamRef === undefined || this.webCamRef.current === null) return;
             var imageSrc = this.webCamRef.current.getScreenshot();
-            console.log(imageSrc);
+            //console.log(imageSrc);
             if (imageSrc !== null) {
                 FetchData('/image/image-all', "post", {
                     "Image": {
@@ -40,7 +39,7 @@ class LiveStream extends React.Component {
                 })
                 .then((data) => {
                     var resp = JSON.parse(data.body);
-                    console.log(resp);
+                    //console.log(resp);
                     if (resp !== null && resp.moderation !== undefined && resp.moderation.length > 0) {
                         this.setState(
                             {webCamModerationLabels: [`${resp.moderation[0].info} (${resp.moderation[0].confidence.toLocaleString(undefined,{style: 'percent', minimumFractionDigits:1})})`]}
@@ -62,12 +61,17 @@ class LiveStream extends React.Component {
                 let min = this.videoRef.current.getCurrentTime() * 1000 - 500;
                 let max = this.videoRef.current.getCurrentTime() * 1000 + 500;
                 var labels = this.moderationLables.find(l=> l.Timestamp >= min && l.Timestamp < max);
-                if (labels !== undefined && labels !== null) {
+                console.log(labels);
+                if (labels !== undefined && labels !== null && labels.ModerationLabel.ParentName.length > 0) {
+                    let vms = Object.assign([], this.state.videoModerationLabels);
+                    let msg = `${labels.Timestamp/1000} s: ${labels.ModerationLabel.ParentName}/${labels.ModerationLabel.Name} (${(labels.ModerationLabel.Confidence/100).toLocaleString(undefined,{style: 'percent', minimumFractionDigits:1})})`;
+                    if (vms.indexOf(msg) == -1)
+                        vms.push(msg);                     
                     this.setState(
                         {
-                            videoModerationLabels: [labels.ModerationLabel.Name + ' (' + (labels.ModerationLabel.Confidence/100).toLocaleString(undefined,{style: 'percent', minimumFractionDigits:1}) + ')']
+                            videoModerationLabels: vms
                         }
-                    )
+                    );
                 }            
             }
         }, 500);
@@ -76,14 +80,21 @@ class LiveStream extends React.Component {
     render() {
         return (
             <div className="livestream">
-                {this.state.videoModerationLabels !== null ? <div className="videoModeration">{this.state.videoModerationLabels}</div>:<div/>}
+                {this.state.videoModerationLabels !== null && this.state.videoModerationLabels.length > 0 ? 
+                    <div className="videoModeration">
+                        {this.state.videoModerationLabels.map((l, i) =>
+                            <p>{l}</p>
+                        )}
+                    </div>
+                :<div/>}
                 <ReactPlayer id="myVideo" 
                     className="bgVideo" 
                     ref={this.videoRef} 
+                    loop={true}
                     playing={true}
                     width="100%"
                     height="100%"
-                    url='https://d2m6vcpsgt3gn5.cloudfront.net/images/media-demo.mov' />
+                    url='https://d2m6vcpsgt3gn5.cloudfront.net/ugc-demo-web/images/media-demo.mp4' />
                 {this.state.webCamModerationLabels !== null && this.state.webCamModerationLabels.length > 0 ? 
                     <div className="webCamModeration">{this.state.webCamModerationLabels[0]}</div>:<div/>
                 }
